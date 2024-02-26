1e7eb1517bf041f3eb31b4674a481ed02e266a59
Mohamad Hleihel <mohammad.helehel@hotmail.com>
2024-02-26 09:16:45 +0100

    new kod try

--- app/src/main/java/com/example/pia12androidfirebase/MainActivity.kt
+++ app/src/main/java/com/example/pia12androidfirebase/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pia12androidfirebase.ui.theme.Pia12androidfirebaseTheme
-
+// skrova kod
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
